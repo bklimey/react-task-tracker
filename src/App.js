1c86d00e69cd143af527b071821af6706ee46332
bklimey <bklim@comp.nus.edu.sg>
2023-05-03 20:28:38 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
+
 
 function App() {
+  const defaultTasks = []
+  const [tasks, setTasks] = useState(defaultTasks);
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    console.log(data);
+    return data;
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    console.log(data);
+    return data;
+  }
+
+  const addTask = async (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(newTask)
+    });
+    const data = await res.json();
+    setTasks([...tasks, data]);
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = {id, ...task};
+    // setTasks([...tasks, newTask]);
+  }
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+    setTasks(tasks.filter((task) => task.id !== id));
+    console.log('delete', id)
+  }
+
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    });
+
+    const data = await res.json();
+
+    setTasks(tasks.map((task) =>
+      task.id === id ?
+        { ...task, reminder: data.reminder }
+        :
+        task
+    ))
+    console.log(id);
+  }
+
   return (
+    <Router>
+      <div className='container'>
+        <Header
+          title='Task Tracker'
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        <Routes>
+          <Route path='/' element={
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {
+                tasks.length > 0 ?
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  /> : 'No tasks'
+              }
+            </>
+          } />
+          <Route path='/about' element={<About />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
+
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +130,7 @@ function App() {
         </a>
       </header>
     </div>
+    */
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Button = ({color, text, onClick}) => {
+
+  return (
+    <button 
+        onClick={onClick} 
+        style={{backgroundColor: color}} 
+        className='btn'
+    >
+        {text}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+    text: '<text>'
+}
+
+// const buttonStyle = {
+//     backgroundColor: {color}
+// }
+
+export default Button
